42f91ab66249e924cc32bcb52e3d54f91e676d3d
Le Thanh <56119673+lethanh050899@users.noreply.github.com>
2021-07-25 22:07:34 +0700

    update Link to course-detail

--- frontend/client/src/components/CourseGridItem/index.js
+++ frontend/client/src/components/CourseGridItem/index.js
@@ -1,4 +1,5 @@
 import react from 'react';
+import {Link} from 'react-router-dom';
 
 const CourseGridItem = (props) => {
     const {course} = props;
@@ -8,20 +9,20 @@ const CourseGridItem = (props) => {
                 <figure className="block-reveal">
                     <div className="block-horizzontal" />
                     <a href="#0" className="wish_bt" />
-                    <a href="course-detail.html"><img src="http://via.placeholder.com/800x533/ccc/fff/course__list_1.jpg" className="img-fluid" alt /></a>
+                    <Link to={`/course-detail/${course.id}`}><img src="http://via.placeholder.com/800x533/ccc/fff/course__list_1.jpg" className="img-fluid" alt /></Link>
                     <div className="price">$54</div>
                     <div className="preview"><span>Preview course</span></div>
                 </figure>
                 <div className="wrapper">
-                    <small>Category</small>
+                    <small>Course Name</small>
                     <h3>{course.courseName}</h3>
-                    <p>Id placerat tacimates definitionem sea, prima quidam vim no. Duo nobis persecuti cu.</p>
+                    <p>{course.shortDescription}</p>
                     <div className="rating"><i className="icon_star voted" /><i className="icon_star voted" /><i className="icon_star voted" /><i className="icon_star" /><i className="icon_star" /> <small>(145)</small></div>
                 </div>
                 <ul>
                     <li><i className="icon_clock_alt" /> 1h 30min</li>
                     <li><i className="icon_like" /> 890</li>
-                    <li><a href="course-detail.html">Enroll now</a></li>
+                    <li><Link to={`/course-detail/${course.id}`}>Enroll now</Link></li>
                 </ul>
             </div>
         </div>
