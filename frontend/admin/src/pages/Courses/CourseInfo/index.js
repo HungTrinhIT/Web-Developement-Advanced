217250aad1910387ef0293c38f443ff391627195
HungTrinh99 <minhhung.it99@gmail.com>
2021-07-26 12:57:12 +0700

    add

---
+++ frontend/admin/src/pages/Courses/CourseInfo/index.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+
+import { Tabs } from "antd";
+import CourseInfoDetail from "./CourseInfoDetail";
+import CourseInfoImage from "./CourseInfoImage";
+import CourseInfoLesson from "./CourseInfoLesson";
+
+const { TabPane } = Tabs;
+const CourseInfo = (props) => {
+  return (
+    <div>
+      <Tabs defaultActiveKey="1" centered>
+        <TabPane tab="Course Infomation" key="1">
+          <CourseInfoDetail />
+        </TabPane>
+        <TabPane tab="Course Image" key="2">
+          <CourseInfoImage />
+        </TabPane>
+        <TabPane tab="Lessons" key="3">
+          <CourseInfoLesson />
+        </TabPane>
+      </Tabs>
+    </div>
+  );
+};
+
+export default CourseInfo;
