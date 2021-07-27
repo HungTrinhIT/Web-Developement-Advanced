448548f7c8c240c96dae6f6d18eeec3f96fcbd8d
HungTrinh99 <minhhung.it99@gmail.com>
2021-07-28 01:58:54 +0700

    updae

--- frontend/admin/src/pages/Courses/CourseInfo/CourseInfoImage/index.js
+++ frontend/admin/src/pages/Courses/CourseInfo/CourseInfoImage/index.js
@@ -1,7 +1,28 @@
-import React from "react";
-
+import React, { useState } from "react";
+import courseApi from "../../../../api/courseApi";
 const CourseInfoImage = () => {
-  return <div></div>;
+  const [img, setImg] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const fd = new FormData();
+    fd.append("image", img);
+    const data = await courseApi.updateImg(1, fd);
+    console.log(data);
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit} encType="multipart/form-data">
+        <input
+          type="file"
+          onChange={(e) => setImg(e.target.files[0])}
+          name="image"
+          accept="image/*"
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default CourseInfoImage;
