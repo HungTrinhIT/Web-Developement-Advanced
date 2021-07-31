f4c2490341ed8ab7e14e5827a7b1f166b48db65d
HungTrinh99 <minhhung.it99@gmail.com>
2021-07-31 12:48:26 +0700

    update

--- frontend/admin/src/App.js
+++ frontend/admin/src/App.js
@@ -5,14 +5,19 @@ import Sidebar from "./layouts/Sidebar";
 import CustomBreadcrumb from "./layouts/Breadcrumb";
 import Navbar from "./layouts/Navbar";
 import CustomFooter from "./layouts/Footer";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import Users from "./pages/Users";
 import Categories from "./pages/Categories";
 import Courses from "./pages/Courses";
 import PageNotFound from "./pages/PageNotFound";
 import AddNewCategory from "./pages/Categories/AddCategory";
-
+import Login from "./pages/Login";
 import AddCourse from "./pages/Courses/AddNewCourse";
 import createAction from "./redux/action/createAction";
 import categoryApi from "./api/categoryApi";
@@ -24,42 +29,42 @@ const routes = [
   {
     exact: true,
     path: "/",
-    component: Home,
+    component: <Home />,
   },
   {
     exact: false,
     path: "/users",
-    component: Users,
+    component: <Users />,
   },
   {
     exact: false,
     path: "/categories",
-    component: Categories,
+    component: <Categories />,
   },
   {
     exact: false,
     path: "/add-category",
-    component: AddNewCategory,
+    component: <AddNewCategory />,
   },
   {
     exact: true,
     path: "/courses",
-    component: Courses,
+    component: <Courses />,
   },
   {
     exact: false,
     path: "/courses/:id",
-    component: CourseInfo,
+    component: <CourseInfo />,
   },
   {
     exact: false,
     path: "/add-course",
-    component: AddCourse,
+    component: <AddCourse />,
   },
   {
-    exact: false,
+    eaxct: false,
     path: "**",
-    component: PageNotFound,
+    component: <PageNotFound />,
   },
 ];
 
@@ -89,14 +94,14 @@ function App(props) {
               style={{ padding: 24, minHeight: 360 }}
             >
               <Switch>
-                {routes.map(({ exact, path, component }, index) => {
+                <Route path="/login" exact={true}>
+                  <Login />
+                </Route>
+                {routes.map(({ component, exact, path }, index) => {
                   return (
-                    <Route
-                      exact={exact}
-                      path={path}
-                      component={component}
-                      key={index}
-                    />
+                    <PrivateRoute exact={exact} path={path} key={index}>
+                      {component}
+                    </PrivateRoute>
                   );
                 })}
               </Switch>
@@ -106,6 +111,24 @@ function App(props) {
         </Layout>
       </Router>
     </Layout>
+  );
+}
+function PrivateRoute({ children, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={() =>
+        localStorage.getItem("elearning_accessToken") ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+            }}
+          />
+        )
+      }
+    />
   );
 }
 
