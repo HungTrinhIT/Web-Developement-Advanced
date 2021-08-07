500b16452c2c27163de04a7e7b469cd972fb7a92
HungTrinh99 <minhhung.it99@gmail.com>
2021-08-07 12:07:53 +0700

    fix rate

--- frontend/client/src/components/CourseInformation/CourseReviews/index.js
+++ frontend/client/src/components/CourseInformation/CourseReviews/index.js
@@ -1,64 +1,54 @@
-import React, { useEffect, useState } from 'react';
-import rateApi from '../../../api/rateApi';
-import { Avatar, Comment, Form, Rate, Input } from 'antd';
-import CourseComment from './CourseComment';
-import { Button } from 'antd/lib/radio';
-
-const desc = ['terrible', 'bad', 'normal', 'good', 'wonderful'];
-
+import React, { useEffect, useState } from "react";
+import rateApi from "../../../api/rateApi";
+import { Avatar, Comment, Form, Rate, Input, Button } from "antd";
+import CourseComment from "./CourseComment";
 
 const CourseReviews = () => {
-    const [form] = Form.useForm();
-    const [rates, setRates] = useState([]);
-    const [value, setValue] = useState([]);
-    useEffect(() => {
-        const fetchAllRate = async () => {
-            try {
-                const rateData = await rateApi.getAll();
-                setRates(rateData.data);
-            } catch (error) {
-                throw error;
-            }
-        };
-        fetchAllRate();
-    }, [])
-
-    const onFinish = (values) => {
-        console.log(values);
+  const [form] = Form.useForm();
+  const [rate, setRate] = useState();
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    const fetchAllRate = async () => {
+      try {
+        const rateData = await rateApi.getAll();
+        setReviews(rateData.data);
+      } catch (error) {
+        throw error;
+      }
     };
+    fetchAllRate();
+  }, []);
 
-    const onHandleRateChange = (value) => {
-        setValue(value);
-    };
-    return (
-        <div>
+  const onFinish = (values) => {
+    // Call API post o day
+  };
 
-            <Form
-                form={form}
-                name="addReview"
-                onFinish={onFinish}
-            >
-                <Form.Item>
-                    <Rate tooltips={desc} onChange={onHandleRateChange} value={value} />
-                    {value ? <span className="ant-rate-text">{desc[value - 1]}</span> : ''}
-                </Form.Item>
-                <Form.Item
-                    name="content"
-                    label="Your Review"
-                >
-                    <Input.TextArea></Input.TextArea>
-                </Form.Item>
-                <Form.Item>
-                    <Button type="primary" htmlType="submit">
-                        Post your review
-                    </Button>
-                </Form.Item>
-            </Form>
+  const onReviewHandleChange = (value) => {
+    setRate(value);
+  };
+  const desc = ["Terrible", "Bad", "Normal", "Good", "Wonderful"];
 
-            {rates.map((rate, index) => {
-                return <CourseComment rate={rate} key={index} />;
-            })}
-        </div>
-    );
+  return (
+    <div>
+      <Form form={form} onFinish={onFinish} name="addNewReview">
+        <Form.Item name="rate">
+          <Rate allowHalf tooltips={desc} onChange={onReviewHandleChange} />
+        </Form.Item>
+        <Form.Item name="content" label="Your review">
+          <Input.TextArea></Input.TextArea>
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Post your review
+          </Button>
+        </Form.Item>
+      </Form>
+
+      {/* List of review */}
+      {reviews.map((rate) => (
+        <CourseComment rate={rate} key={rate.id} />
+      ))}
+    </div>
+  );
 };
-export default CourseReviews
+export default CourseReviews;
