871d02811cd1a0b85d228f5234b66b0c288bfc82
HungTrinh99 <minhhung.it99@gmail.com>
2021-08-24 21:04:34 +0700

    update

--- frontend/client/src/components/CourseInformation/CourseLessons/index.js
+++ frontend/client/src/components/CourseInformation/CourseLessons/index.js
@@ -1,66 +1,51 @@
-import React, { useEffect, useState } from 'react';
-import { useHistory, useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useHistory, useParams } from "react-router-dom";
 import { connect } from "react-redux";
-import lessonApi from '../../../api/lessonApi';
-import LessonItem from './LessonItem';
-import purchaseApi from '../../../api/purchaseApi';
+import lessonApi from "../../../api/lessonApi";
+import LessonItem from "./LessonItem";
+import purchaseApi from "../../../api/purchaseApi";
 
-const CourseLessons = ({ course, user, ...props }) => {
-    const [lesson, setLesson] = useState([]);
-    const { userInfo, isAuthenticated } = user;
-    const [purchaseStatus, setPurchaseStatus] = useState(false);
-    useEffect(() => {
-        const fetchAllLesson = async () => {
-            try {
-                const lessonData = await lessonApi.getAllForTeacher(course.id);
-                setLesson(lessonData.data);
-            } catch (error) {
-                throw error;
-            }
-        };
-        fetchAllLesson();
-    }, []);
+const CourseLessons = ({ course, user, purchaseStatus, ...props }) => {
+  const [lessons, setLesson] = useState([]);
 
-    useEffect(() => {
-        const fetchPurchase = async () => {
-            try {
-                if (isAuthenticated) {
-                    const purchaseData = await purchaseApi.singleByBothID(course.id, userInfo.id);
-                    if (purchaseData.data.isExist === true) {
-                        setPurchaseStatus(true);
-                    }
-                    else {
-                        setPurchaseStatus(false);
-                    }
-                }
-                else { }
-            } catch (error) {
-                throw error;
-            }
-        };
-        fetchPurchase();
-    }, []);
-    return (
-        <div>
-            <div className="intro_title">
-                <h2>Lessons</h2>
-            </div>
-            <div className="card-body">
-                <div className="list_lessons">
-                    <ul>
-                        {lesson.map((lessonItem, index) => {
-                            return <LessonItem purchase={purchaseStatus} lesson={lessonItem} key={index} />;
-                        })}
-                        
-                    </ul>
-                </div>
-            </div>
+  useEffect(() => {
+    const fetchAllLesson = async () => {
+      try {
+        const lessonData = await lessonApi.getAllForTeacher(course.id);
+        setLesson(lessonData.data);
+      } catch (error) {
+        throw error;
+      }
+    };
+    fetchAllLesson();
+  }, []);
+
+  return (
+    <div>
+      <div className="intro_title">
+        <h2>Lessons</h2>
+      </div>
+      <div className="card-body">
+        <div className="list_lessons">
+          <ul>
+            {lessons.map((lessonItem, index) => {
+              return (
+                <LessonItem
+                  purchase={purchaseStatus}
+                  lesson={lessonItem}
+                  key={index}
+                />
+              );
+            })}
+          </ul>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 const mapStateToProps = (state) => {
-    return {
-        user: state.users,
-    };
+  return {
+    user: state.users,
+  };
 };
 export default connect(mapStateToProps)(CourseLessons);
